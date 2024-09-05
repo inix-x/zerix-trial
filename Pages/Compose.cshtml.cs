f459491439ca13d0bed9d942ff431db3c591b346
Omar Gerardo <omargrard@gmail.com>
2024-09-06 05:25:22 +0800

    feat: added compose design

--- Pages/Compose.cshtml.cs
+++ Pages/Compose.cshtml.cs
@@ -1,10 +1,15 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Http; 
 
 namespace zerix_trial.Pages;
 
 public class ComposeModel : PageModel
 {
+    [BindProperty]
+    public List<IFormFile> UploadedFiles { get; set; } = new List<IFormFile>();
+
     private readonly ILogger<ComposeModel> _logger;
 
     public ComposeModel(ILogger<ComposeModel> logger)
@@ -16,4 +21,18 @@ public class ComposeModel : PageModel
     {
 
     }
+
+    public IActionResult OnPostUploadFiles(List<IFormFile> files)
+        {
+            if (files != null && files.Count > 0)
+            {
+                foreach (var file in files)
+                {
+                    UploadedFiles.Add(file);
+                }
+            }
+            
+            // Redirect to Get request to reset form state
+            return RedirectToPage();
+        }
 }
